a0d22735796282ee94efc6721b111d2da4ecb1dc
Geoff Turk <geoff@ic3.dev>
2020-12-03 09:57:41 +0100

    Check if dirpath

--- api/schemas.js
+++ api/schemas.js
@@ -2,16 +2,16 @@ const path = require("path");
 const fs = require("fs");
 
 module.exports = (_, res) => {
-  let schemas = [];
+  // let schemas = [];
   const directoryPath = path.join(__dirname, "schemas");
-
-  fs.readdir(directoryPath, function (err, files) {
-    if (err) {
-      return console.log("Unable to scan directory: " + err);
-    }
-    files.forEach(function (file) {
-      schemas.push(file);
-    });
-    return res.status(200).send(schemas);
-  });
+  return res.status(200).send(directoryPath);
+  // fs.readdir(directoryPath, function (err, files) {
+  //   if (err) {
+  //     return console.log("Unable to scan directory: " + err);
+  //   }
+  //   files.forEach(function (file) {
+  //     schemas.push(file);
+  //   });
+  //   return res.status(200).send(schemas);
+  // });
 };
