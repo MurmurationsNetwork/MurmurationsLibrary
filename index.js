b37f76214dccd9a2ea61bff0ecdc3650a3bcc95f
Geoff Turk <geoff@ic3.dev>
2020-12-03 10:58:37 +0100

    Use preview URL

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const fetch = require("node-fetch");
 
 let schemas = [];
 
-fetch("https://cdn.murmurations.tech/schemas")
+// fetch("https://cdn.murmurations.tech/schemas")
+fetch("https://murmurations-library-5dq2s0fbp.vercel.app")
   .then((res) => res.text())
   .then((body) => {
     const files = [...body.matchAll(/(?<=file json">)(.*)(?=<\/a>)/g)];
