e91d655d357b77aa561e9ed975c96b928cd3cd9e
Geoff Turk <32190662+geoffturk@users.noreply.github.com>
2020-12-03 11:17:53 +0100

    Add schema filename API (#8)

---
+++ api/schemas.js
@@ -0,0 +1,20 @@
+const fetch = require('node-fetch')
+
+let schemas = []
+
+module.exports = (_, res) => {
+  fetch('https://cdn.murmurations.tech/schemas')
+    .then((res) => res.text())
+    .then((body) => {
+      const files = [...body.matchAll(/(?<=file json">)(.*)(?=<\/a>)/g)]
+
+      files.forEach((file) => {
+        if (schemas.includes(file[0])) return
+        schemas.push(file[0])
+      })
+    })
+
+  res.status(200)
+  res.setHeader('Content-Type', 'application/json')
+  res.end(JSON.stringify(schemas, null, 2))
+}
