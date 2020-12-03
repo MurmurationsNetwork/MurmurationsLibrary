e5c7eaf39f74ff3045656c348f0c4f2048525560
Geoff Turk <geoff@ic3.dev>
2020-12-03 10:40:26 +0100

    Get and grep hack

--- api/schemas.js
+++ api/schemas.js
@@ -1,17 +1,33 @@
-const path = require("path");
-const fs = require("fs");
+// const path = require("path");
+// const fs = require("fs");
 
-module.exports = (_, res) => {
-  // let schemas = [];
-  const directoryPath = path.join(__dirname, "schemas");
-  return res.status(200).send(directoryPath);
-  // fs.readdir(directoryPath, function (err, files) {
-  //   if (err) {
-  //     return console.log("Unable to scan directory: " + err);
-  //   }
-  //   files.forEach(function (file) {
-  //     schemas.push(file);
-  //   });
-  //   return res.status(200).send(schemas);
-  // });
-};
+// module.exports = (_, res) => {
+//   // let schemas = [];
+//   const directoryPath = path.join(__dirname, "schemas");
+//   return res.status(200).send(directoryPath);
+//   // fs.readdir(directoryPath, function (err, files) {
+//   //   if (err) {
+//   //     return console.log("Unable to scan directory: " + err);
+//   //   }
+//   //   files.forEach(function (file) {
+//   //     schemas.push(file);
+//   //   });
+//   //   return res.status(200).send(schemas);
+//   // });
+// };
+
+const fetch = require("node-fetch");
+
+let schemas = [];
+
+fetch("https://cdn.murmurations.tech/schemas")
+  .then((res) => res.text())
+  .then((body) => {
+    const files = [...body.matchAll(/(?<=file json">)(.*)(?=<\/a>)/g)];
+
+    files.forEach((file) => {
+      schemas.push(file[0]);
+    });
+
+    console.log(JSON.stringify(schemas, null, 2));
+  });
