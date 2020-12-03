f8d5ca251734eebf1f7dfee27c3aceaebc52d335
Geoff Turk <geoff@ic3.dev>
2020-12-03 10:52:05 +0100

    Return json

--- api/schemas.js
+++ api/schemas.js
@@ -27,9 +27,12 @@ module.exports = (_, res) => {
       const files = [...body.matchAll(/(?<=file json">)(.*)(?=<\/a>)/g)];
 
       files.forEach((file) => {
+        if (schemas.includes(file)) return;
         schemas.push(file[0]);
       });
     });
 
-  return res.status(200).send(JSON.stringify(schemas, null, 2));
+  res.status(200);
+  res.setHeader("Content-Type", "application/json");
+  res.end(JSON.stringify(schemas, null, 2));
 };
