59743f4a7e27c2321d1934d6de1a18e1b15b32f1
Geoff Turk <geoff@ic3.dev>
2020-12-04 10:46:15 +0100

    Add last commit date

---
+++ api/schemas-2.js
@@ -0,0 +1,42 @@
+const fetch = require('node-fetch')
+
+async function getGithubLastCommitTime() {
+  const response = await fetch(
+    'https://api.github.com/repos/MurmurationsNetwork/MurmurationsLibrary/commits'
+  )
+  const data = await response.json()
+  return data[0].commit.author.date
+}
+
+async function getSchemaList() {
+  let schemaList = []
+  const response = await fetch('https://cdn.murmurations.tech/schemas')
+  const data = await response.text()
+  const files = [...data.matchAll(/(?<=file json">)(.*)(?=.json<\/a>)/g)]
+
+  files.forEach((file) => {
+    if (schemaList.includes(file[0])) return
+    schemaList.push(file[0])
+  })
+  return schemaList
+}
+
+async function createSchemasResponse() {
+  const response = {}
+  const lastCommit = await getGithubLastCommitTime()
+  const schemaList = await getSchemaList()
+
+  response.lastCommit = await lastCommit
+  response.schemaList = await schemaList
+
+  return JSON.stringify(response, null, 2)
+}
+
+module.exports = (_, res) => {
+  createSchemasResponse().then((response) => {
+    res.status(200)
+    res.setHeader('Content-Type', 'application/json')
+    res.setHeader('Cache-Control', 's-maxage=1, stale-while-revalidate')
+    res.end(response)
+  })
+}
