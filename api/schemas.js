6e42930d11ed2df7c954e8983453a67fd1ede352
Geoff Turk <32190662+geoffturk@users.noreply.github.com>
2020-12-06 22:39:57 +0100

    Update schema list serverless function (#9)

--- api/schemas.js
+++ api/schemas.js
@@ -1,23 +1,52 @@
 const fetch = require('node-fetch')
 
-let schemaList = []
+async function getGithubLastCommitTime() {
+  const response = await fetch(
+    'https://api.github.com/repos/MurmurationsNetwork/MurmurationsLibrary/commits'
+  )
+  if (response.status !== 200)
+    throw Error(`{"error": "${response.status} - ${response.url}"}`)
+  const data = await response.json()
+  return data[0].commit.author.date
+}
+
+async function getSchemaList() {
+  let schemaList = []
+  const response = await fetch('https://cdn.murmurations.tech/schemas')
+  if (response.status !== 200)
+    throw Error(`{"error": "${response.status} - ${response.url}"}`)
+  const data = await response.text()
+  const files = [...data.matchAll(/(?<=file json">)(.*)(?=.json<\/a>)/g)]
+
+  files.forEach((file) => {
+    if (schemaList.includes(file[0])) return
+    schemaList.push(file[0])
+  })
+  return schemaList
+}
+
+async function createSchemasResponse() {
+  const response = {}
+  const lastCommit = await getGithubLastCommitTime()
+  const schemaList = await getSchemaList()
+
+  response.last_commit = await lastCommit
+  response.schema_list = await schemaList
+
+  return JSON.stringify(response, null, 2)
+}
 
 module.exports = (_, res) => {
-  fetch('https://cdn.murmurations.tech/schemas')
-    .then((res) => res.text())
-    .then((body) => {
-      const files = [...body.matchAll(/(?<=file json">)(.*)(?=.json<\/a>)/g)]
-
-      files.forEach((file) => {
-        if (schemaList.includes(file[0])) return
-        schemaList.push(file[0])
-      })
-      return schemaList
-    })
-    .then((schemas) => {
+  createSchemasResponse()
+    .then((response) => {
       res.status(200)
       res.setHeader('Content-Type', 'application/json')
       res.setHeader('Cache-Control', 's-maxage=1, stale-while-revalidate')
-      res.end(JSON.stringify(schemas, null, 2))
+      res.end(response)
+    })
+    .catch((err) => {
+      res.status(500)
+      res.setHeader('Content-Type', 'application/json')
+      res.end(err.message)
     })
 }
