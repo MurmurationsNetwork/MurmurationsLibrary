4113986d74291c9bb25e1bc221e9e6f29b011c0e
Geoff Turk <geoff@ic3.dev>
2020-12-06 22:23:16 +0100

    Add error handling

--- api/schemas-2.js
+++ api/schemas-2.js
@@ -4,6 +4,8 @@ async function getGithubLastCommitTime() {
   const response = await fetch(
     'https://api.github.com/repos/MurmurationsNetwork/MurmurationsLibrary/commits'
   )
+  if (response.status !== 200)
+    throw Error(`${response.status} - ${response.url}`)
   const data = await response.json()
   return data[0].commit.author.date
 }
@@ -11,6 +13,8 @@ async function getGithubLastCommitTime() {
 async function getSchemaList() {
   let schemaList = []
   const response = await fetch('https://cdn.murmurations.tech/schemas')
+  if (response.status !== 200)
+    throw Error(`${response.status} - ${response.url}`)
   const data = await response.text()
   const files = [...data.matchAll(/(?<=file json">)(.*)(?=.json<\/a>)/g)]
 
@@ -33,10 +37,12 @@ async function createSchemasResponse() {
 }
 
 module.exports = (_, res) => {
-  createSchemasResponse().then((response) => {
-    res.status(200)
-    res.setHeader('Content-Type', 'application/json')
-    res.setHeader('Cache-Control', 's-maxage=1, stale-while-revalidate')
-    res.end(response)
-  })
+  createSchemasResponse()
+    .then((response) => {
+      res.status(200)
+      res.setHeader('Content-Type', 'application/json')
+      res.setHeader('Cache-Control', 's-maxage=1, stale-while-revalidate')
+      res.end(response)
+    })
+    .catch((err) => console.error('Schema List Error - ', err.message))
 }
