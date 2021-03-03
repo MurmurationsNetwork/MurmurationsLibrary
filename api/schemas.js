bb653c79e01302b2aa9afbbd3ab91fac6d6c0529
Geoff Turk <geoff@ic3.dev>
2021-03-03 14:41:59 +0100

    6

--- api/schemas.js
+++ api/schemas.js
@@ -42,7 +42,7 @@ module.exports = (req, res) => {
       res.status(200)
       res.setHeader('Content-Type', 'application/json')
       res.setHeader('Cache-Control', 's-maxage=1, stale-while-revalidate')
-      res.end(JSON.stringify(req, null, 2))
+      res.end(req.body)
     })
     .catch((err) => {
       res.status(500)
