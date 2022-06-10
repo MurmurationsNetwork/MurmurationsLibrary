6fa13bd91c0f0aa4dc338b1a57fb3352767dd48d
Geoff Turk <geoff@ic3.dev>
2022-06-10 12:37:09 +0200

    Set up main branch fields and schemas

--- api/schemas.js
+++ api/schemas.js
@@ -1,58 +1,58 @@
-const fetch = require('node-fetch')
+const fetch = require("node-fetch");
 
 async function getGithubLastCommitTime(host) {
   const response = await fetch(
-    host === 'cdn.murmurations.network'
-      ? 'https://api.github.com/repos/MurmurationsNetwork/MurmurationsLibrary/commits'
-      : 'https://api.github.com/repos/MurmurationsNetwork/MurmurationsLibrary/commits?sha=staging'
-  )
+    host === "cdn.murmurations.network"
+      ? "https://api.github.com/repos/MurmurationsNetwork/MurmurationsLibrary/commits"
+      : "https://api.github.com/repos/MurmurationsNetwork/MurmurationsLibrary/commits?sha=staging"
+  );
   if (response.status !== 200)
-    throw Error(`{"error": "${response.status} - ${response.url}"}`)
-  const data = await response.json()
-  return data[0].commit.author.date
+    throw Error(`{"error": "${response.status} - ${response.url}"}`);
+  const data = await response.json();
+  return data[0].commit.author.date;
 }
 
 async function getSchemaList(host) {
-  let schemaList = []
+  let schemaList = [];
   const response = await fetch(
-    host === 'cdn.murmurations.network'
-      ? 'https://cdn.murmurations.network/schemas'
-      : 'https://test-cdn.murmurations.network/schemas'
-  )
+    host === "cdn.murmurations.network"
+      ? "https://cdn.murmurations.network/schemas"
+      : "https://main-cdn.murmurations.network/schemas"
+  );
   if (response.status !== 200)
-    throw Error(`{"error": "${response.status} - ${response.url}"}`)
-  const data = await response.text()
-  const files = [...data.matchAll(/(?<=file json">)(.*)(?=.json<\/a>)/g)]
+    throw Error(`{"error": "${response.status} - ${response.url}"}`);
+  const data = await response.text();
+  const files = [...data.matchAll(/(?<=file json">)(.*)(?=.json<\/a>)/g)];
 
   files.forEach((file) => {
-    if (schemaList.includes(file[0])) return
-    schemaList.push(file[0])
-  })
-  return schemaList
+    if (schemaList.includes(file[0])) return;
+    schemaList.push(file[0]);
+  });
+  return schemaList;
 }
 
 async function createSchemasResponse(host) {
-  const response = {}
-  const lastCommit = await getGithubLastCommitTime(host)
-  const schemaList = await getSchemaList(host)
+  const response = {};
+  const lastCommit = await getGithubLastCommitTime(host);
+  const schemaList = await getSchemaList(host);
 
-  response.last_commit = await lastCommit
-  response.schema_list = await schemaList
+  response.last_commit = await lastCommit;
+  response.schema_list = await schemaList;
 
-  return JSON.stringify(response, null, 2)
+  return JSON.stringify(response, null, 2);
 }
 
 module.exports = (req, res) => {
   createSchemasResponse(req.headers.host)
     .then((response) => {
-      res.status(200)
-      res.setHeader('Content-Type', 'application/json')
-      res.setHeader('Cache-Control', 's-maxage=1, stale-while-revalidate')
-      res.end(response)
+      res.status(200);
+      res.setHeader("Content-Type", "application/json");
+      res.setHeader("Cache-Control", "s-maxage=1, stale-while-revalidate");
+      res.end(response);
     })
     .catch((err) => {
-      res.status(500)
-      res.setHeader('Content-Type', 'application/json')
-      res.end(err.message)
-    })
-}
+      res.status(500);
+      res.setHeader("Content-Type", "application/json");
+      res.end(err.message);
+    });
+};
